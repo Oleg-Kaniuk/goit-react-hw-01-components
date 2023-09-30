e50f2b92e1475b788183549dc788fb753929df66
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-09-30 14:31:45 +0300

    Statistics css

--- src/components/StatList/StatList.jsx
+++ src/components/StatList/StatList.jsx
@@ -5,7 +5,7 @@ export const StatList = ({ stats }) => {
     return (
         <ul className={styles.statList}>
             {stats.map( ({id, label, percentage}) => {
-                return (<li key={id} className={styles.item} style={{backgroundColor: `${getRandomHexColor()}`}}>
+                return (<li key={id} className={styles.element} style={{backgroundColor: `${getRandomHexColor()}`}}>
                     <span className="label">{label}</span>
                     <span className="percentage">{percentage}%</span>
             </li>)
